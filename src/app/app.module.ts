fa8135961443424ce640a19e7ea5c09aafb3839f
20011973DEVCL <danilo.alarcon.lopez@icloud.com>
2022-10-26 22:31:45 -0300

    se implementa sumar y reestar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { contadorReducer } from './contador/contador.reducer';
+
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
-import { StoreModule } from '@ngrx/store';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +18,7 @@ import { StoreModule } from '@ngrx/store';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({ contador: contadorReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
