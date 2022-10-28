78689117513b3edec5bfd0f777444a12e4591b62
20011973DEVCL <danilo.alarcon.lopez@icloud.com>
2022-10-27 22:37:23 -0300

    optimizando aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,21 @@ interface AppState {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   contador: number = 0;
 
   constructor(private store: Store<AppState>) {
 
-    this.store.subscribe(state => {
-      this.contador =  state.contador;
-    })
-  }
+    this.store .select('contador')
+      .subscribe(contador => this.contador =  contador);
+    }
 
-  sumar() {
-    this.store.dispatch(actions.incrementar());
-  }
+    sumar() {
+      this.store.dispatch(actions.incrementar());
+    }
 
-  restar() {
-    this.store.dispatch(actions.decrementar());
+    restar() {
+      this.store.dispatch(actions.decrementar());
+    }
   }
-}
